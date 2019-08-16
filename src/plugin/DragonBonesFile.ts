a316f3d3f4bd9b4308c39548c3de5cf7bfc2360b
huhaitao <huhaitao@100tal.com>
2019-08-16 14:08:38 +0800

    add pendingDestroy

--- src/plugin/DragonBonesFile.ts
+++ src/plugin/DragonBonesFile.ts
@@ -85,6 +85,7 @@ export class DragonBonesFile extends Phaser.Loader.MultiFile {
       json.addToCache();
       bone.addToCache();
       this.loader.textureManager.addImage(image.key, image.data);
+      image.pendingDestroy(); // call pendingDestroy manually
 
       this.complete = true;
     }
